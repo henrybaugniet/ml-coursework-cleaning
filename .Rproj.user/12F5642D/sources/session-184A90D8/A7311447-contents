29888cca3d9bbf30435baad4b2b6fa9e4a824413
Henry Baugniet <henry@theracingmanager.com>
2022-12-04 17:58:05 +0800

    initial commit

---
+++ .Rproj.user/12F5642D/sources/session-184A90D8/A7311447-contents
@@ -0,0 +1,6 @@
+
+### Progeny Variables
+
+- Median RPR 
+- RPR100 Strike Rate
+- Maiden Strike Rate
---
+++ params.R
@@ -0,0 +1,5 @@
+# Parameters
+
+# File Paths
+RI_DAILY_PATH <- file.path(Sys.getenv("RRDIR"), "RI_daily", 'Data', 'allRISetUp.RData')
+SALE_CATALOGUES_PATH <- file.path(Sys.getenv("RRDIR"), "RawCataloguesRepo")
